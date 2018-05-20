5c3ce50248fe1c9bfa072fd5fbf9dae0941fb30c
Rubén Marín <webdrmarin@gmail.com>
2018-05-20 12:46:12 +0200

    Conectar usando la IP pública del servidor

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const net = require('net');
-const port = 3000;
+const publicIp = require('public-ip');
+// Configuración del nodo
+const config = {};
+config.port = 3000;
+
+// IPv4
+publicIp.v4().then(ip => {
+  config.publicIp = ip;
+  initServer();
+  initClient();
+});
+
 // Crear servidor y escuchar conexiones
-const server = net.createServer((socket) => {
-  socket.setEncoding('utf8');
-  socket.on('data', (data) => {
-    console.log(data);
-  });
-}).listen(port);
+const initServer = () => {
+  const server = net.createServer((socket) => {
+    // Codificación utf8
+    socket.setEncoding('utf8');
+    socket.on('data', (data) => {
+      console.log(data);
+    });
+  }).listen({port: config.port});
+}
 
 // Crear conexión
-const client = net.createConnection((port), () => {
-  // Enviar instrucciones
-  client.write('getNodes');
-});
+const initClient = () => {
+  const client = net.createConnection(
+    {host: config.publicIp, port: config.port}, () => {
+    // Enviar instrucciones
+    client.write('getNodes');
+  });
+}
